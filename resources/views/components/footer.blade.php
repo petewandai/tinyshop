2fdd30d0e24a9d1dd4f5f04fa2761b2d99bd9172
Peter Wandai Jnr <29979768+petewandai@users.noreply.github.com>
2023-09-21 10:01:01 +0200

    Initial commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-gray-50">
+    <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
+        <x-brand.logo class="w-auto h-8 text-indigo-600" />
+
+        <p class="pt-4 mt-4 text-sm text-gray-500 border-t border-gray-100">
+            &copy; {{ now()->year }} Tiny Shop Namibia
+        </p>
+    </div>
+</footer>
---
+++ resources/views/components/welcome-banner.blade.php
@@ -0,0 +1,26 @@
+<section class="bg-gray-50">
+    <div class="max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-xl mx-auto text-center">
+            <h1 class="text-3xl font-extrabold sm:text-5xl">
+                Welcome to the
+
+                <span class="text-indigo-600">
+                    Tiny Shop
+                </span>
+
+                Online Store
+
+                <span role="img"
+                      aria-hidden="true">
+                    👋
+                </span>
+            </h1>
+
+            <p class="mt-4 font-medium sm:leading-relaxed sm:text-xl">
+                Our online shop hosts a wide array of products from different suppliers.
+                We accomodate all brands, materials and provide free shipping in Namibia!
+            </p>
+
+        </div>
+    </div>
+</section>
